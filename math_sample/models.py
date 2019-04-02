c3a9ee5287dda2dab6626c0b39a16b30cd6fff4c
vlad <vladshatalyuk@gmail.com>
2019-04-02 19:21:15 +0300

    added model Class and other staff

--- math_sample/models.py
+++ math_sample/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=200)
+    file_question = models.FileField(upload_to='question', null=True)
+    file_answers = models.FileField(upload_to='answers', null=True)
